4400f6087f380b17bfe405fd691f1e1228b68a76
n.toussaint <n.toussaint@etu.unistra.fr>
2018-07-07 18:48:22 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# mucaa
+
+## Installation
+
+One command to rule them all: `pipenv install --dev`
+
+## Usage
+
+Log into the pipenv shell and run the flask debug server:
+```bash
+pipenv shell
+python app.py
+```
---
+++ app.py
@@ -0,0 +1,38 @@
+import os
+import vlc
+import glob
+import random
+
+from flask import Flask, render_template, jsonify, request, send_from_directory
+
+app = Flask(__name__, static_url_path='/static/')
+
+@app.route("/")
+def index():
+
+  samples = glob.glob('static/audio/*.mp3')
+  data = []
+  for sample in samples:
+    sample = os.path.splitext(os.path.basename(sample))[0]
+    data.append((sample, sample.title()))
+
+  return render_template("index.html", samples=data)
+
+
+@app.route("/play/<sample>")
+def play(sample):
+  samples = glob.glob('static/audio/{}.mp3'.format(sample))
+  sample = random.choice(samples)
+
+  p = vlc.MediaPlayer(sample)
+  p.audio_set_volume(100)
+  p.play()
+
+  return ('', 204)
+
+# @app.route('/static/<path:path>')
+# def serve_static(path):
+#     return send_from_directory('static/', path)
+
+if __name__ == "__main__":
+    app.run(debug=True)
