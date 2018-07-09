b9a3f30abd529869003bf9939b33a18cdf2ea33a
n.toussaint <n.toussaint@etu.unistra.fr>
2018-07-09 15:37:14 +0100

    Polish stuff

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def serve_static(path):
     return send_from_directory('static/', path)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')#, port=80)
+    app.run(debug=True, host='0.0.0.0')
